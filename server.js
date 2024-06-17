f59f35c668a85b042fd1658722e6910e6f2b7002
Felix Kirchhoff <f.kirchhoff@proton.me>
2024-06-17 14:53:40 +0200

    update node server version

--- server.js
+++ server.js
@@ -2,21 +2,29 @@ import { createServer } from "node:http";
 import { jokes } from "./lib/data";
 
 const server = createServer((request, response) => {
+  const headers = {
+    "Access-Control-Allow-Origin": "*",
+  };
+
   if (request.url === "/api/bad-jokes") {
+    response.writeHead(200, headers);
     response.end(JSON.stringify(jokes));
     return;
   }
 
   if (request.url === "/api/bad-jokes/0") {
     const joke = jokes.find((joke) => joke.id === "0");
+
+    response.writeHead(200, headers);
     response.end(JSON.stringify(joke));
     return;
   }
 
   if (request.url.startsWith("/api/bad-jokes/")) {
     const id = request.url.split("/")[3];
-
     const joke = jokes.find((joke) => joke.id === id);
+
+    response.writeHead(200, headers);
     response.end(JSON.stringify(joke));
     return;
   }
